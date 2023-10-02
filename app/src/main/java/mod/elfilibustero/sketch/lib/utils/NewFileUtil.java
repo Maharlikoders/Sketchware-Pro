f24455b520aab7ee1b00d6e35feaf0e4d97b9605
Maharlikoders <maharlikoders.ph@gmail.com>
2023-10-02 12:13:55 +0800

    feat: Added new methods to get the size of folder or file

--- app/src/main/java/mod/elfilibustero/sketch/lib/utils/NewFileUtil.java
+++ app/src/main/java/mod/elfilibustero/sketch/lib/utils/NewFileUtil.java
@@ -2,16 +2,50 @@ package mod.elfilibustero.sketch.lib.utils;
 
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.FileVisitOption;
 import java.nio.file.FileVisitResult;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.SimpleFileVisitor;
 import java.nio.file.StandardCopyOption;
 import java.nio.file.attribute.BasicFileAttributes;
+import java.util.EnumSet;
 import java.util.HashSet;
 import java.util.Set;
 
 public class NewFileUtil {
+
+	public static long getSize(String path) throws IOException {
+		Path paths = Paths.get(path);
+
+		if (Files.isDirectory(paths)) {
+			return getFolderSize(path);
+		} else if (Files.isRegularFile(paths)) {
+			return Files.size(paths);
+		} else {
+			return 0;
+		}
+	}
+
+	public static long getFolderSize(String path) throws IOException {
+		Path folder = Paths.get(path);
+		long size = 0;
+
+		if (Files.isDirectory(folder)) {
+			EnumSet<FileVisitOption> options = EnumSet.of(FileVisitOption.FOLLOW_LINKS);
+			Files.walk(folder, options, Integer.MAX_VALUE)
+				.filter(Files::isRegularFile)
+				.forEach(file -> {
+					try {
+						size += Files.size(file);
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+				});
+		}
+
+		return size;
+	}
 
 	public static void copyFiles(String source, String destination, Set<String> exclusions) throws IOException {
 		Path sourceDir = Paths.get(source);
