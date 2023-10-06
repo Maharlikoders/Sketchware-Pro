5eddd45bbef35a88fe2aae6f0ff44302f72e175d
Maharlikoders <maharlikoders.ph@gmail.com>
2023-10-06 14:20:00 +0800

    feat: Added new activity manage external add library

---
+++ app/src/main/java/mod/elfilibustero/sketch/editor/manage/library/external/ManageExternalAddLibraryActivity.java
@@ -0,0 +1,84 @@
+package mod.elfilibustero.sketch.editor.manage.library.external;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Switch;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.appbar.MaterialToolbar;
+
+
+import a.a.a.aB;
+import a.a.a.wB;
+
+import com.sketchware.pro.R;
+import com.sketchware.pro.databinding.ManageExternalAddLibraryBinding;
+
+import mod.SketchwareUtil;
+import mod.hey.studios.util.Helper;
+
+public class ManageExternalAddLibraryActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private ManageExternalAddLibraryBinding binding;
+    private String sc_id;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ManageExternalAddLibraryBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        if (savedInstanceState == null) {
+            sc_id = getIntent().getStringExtra("sc_id");
+        } else {
+            sc_id = savedInstanceState.getString("sc_id");
+        }
+        init();
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        outState.putString("sc_id", sc_id);
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        if (id == R.id.layout_switch) {
+            Switch switchLib = binding.switchLib;
+            switchLib.setChecked(!switchLib.isChecked());
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem menuItem) {
+        return super.onOptionsItemSelected(menuItem);
+    }
+
+    private void init() {
+        MaterialToolbar toolbar = binding.toolbar;
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Add New External Library");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowTitleEnabled(true);
+        toolbar.setNavigationOnClickListener(Helper.getBackPressedClickListener(this));
+        binding.layoutSwitch.setOnClickListener(this);
+    }
+}
