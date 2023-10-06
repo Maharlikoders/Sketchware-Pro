7e5e394de09b5f5223a06c50ba52d06e2e044851
Maharlikoders <maharlikoders.ph@gmail.com>
2023-10-06 13:37:47 +0800

    fix: import intent class

--- app/src/main/java/mod/elfilibustero/sketch/editor/manage/library/external/ManageExternalLibraryActivity.java
+++ app/src/main/java/mod/elfilibustero/sketch/editor/manage/library/external/ManageExternalLibraryActivity.java
@@ -1,6 +1,7 @@
 package mod.elfilibustero.sketch.editor.manage.library.external;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
