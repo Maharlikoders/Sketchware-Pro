289b779becfb77c8bf4e0c73cbb5df4b9ccabc6e
Maharlikoders <140351419+Maharlikoders@users.noreply.github.com>
2023-10-09 22:05:03 +0800

    Update ResourceCompiler.java

--- app/src/main/java/mod/jbk/build/compiler/resource/ResourceCompiler.java
+++ app/src/main/java/mod/jbk/build/compiler/resource/ResourceCompiler.java
@@ -123,6 +123,9 @@ public class ResourceCompiler {
             compileBuiltInLibraryResources();
             LogUtil.d(TAG + ":c", "Compiling built-in library resources took " + (System.currentTimeMillis() - savedTimeMillis) + " ms");
             savedTimeMillis = System.currentTimeMillis();
+            compileExternalLibraryResources(outputPath);
+            LogUtil.d(TAG + ":c", "Compiling external library resources took " + (System.currentTimeMillis() - savedTimeMillis) + " ms");
+            savedTimeMillis = System.currentTimeMillis();
             compileLocalLibraryResources(outputPath);
             LogUtil.d(TAG + ":c", "Compiling local library resources took " + (System.currentTimeMillis() - savedTimeMillis) + " ms");
             savedTimeMillis = System.currentTimeMillis();
@@ -328,6 +331,34 @@ public class ResourceCompiler {
                     commands.add(localLibraryResDirectory);
                     commands.add("-o");
                     commands.add(outputPath + File.separator + localLibraryDirectory.getName() + ".zip");
+
+                    LogUtil.d(TAG + ":cLLR", "Now executing: " + commands);
+                    BinaryExecutor executor = new BinaryExecutor();
+                    executor.setCommands(commands);
+                    if (!executor.execute().isEmpty()) {
+                        LogUtil.e(TAG, executor.getLog());
+                        throw new zy(executor.getLog());
+                    }
+                }
+            }
+        }
+        
+        private void compileExternalLibraryResources(String outputPath) throws zy, MissingFileException {
+            int externalLibrariesCount = buildHelper.externalLibraryHandler.get(ExternalLibraryHandler.ResourceType.RES).size();
+            LogUtil.d(TAG + ":cLLR", "About to compile " + externalLibrariesCount
+                    + " local " + (externalLibrariesCount == 1 ? "library" : "libraries"));
+            for (String externalLibraryResDirectory : buildHelper.externalLibraryHandler.get(ExternalLibraryHandler.ResourceType.RES)) {
+                File externalLibraryDirectory = new File(externalLibraryResDirectory).getParentFile();
+                if (externalLibraryDirectory != null) {
+                    compilingAssertDirectoryExists(externalLibraryResDirectory);
+
+                    ArrayList<String> commands = new ArrayList<>();
+                    commands.add(aapt2.getAbsolutePath());
+                    commands.add("compile");
+                    commands.add("--dir");
+                    commands.add(externalLibraryResDirectory);
+                    commands.add("-o");
+                    commands.add(outputPath + File.separator + externalLibraryDirectory.getName() + ".zip");
 
                     LogUtil.d(TAG + ":cLLR", "Now executing: " + commands);
                     BinaryExecutor executor = new BinaryExecutor();
