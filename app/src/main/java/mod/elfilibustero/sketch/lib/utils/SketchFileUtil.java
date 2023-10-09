2633b9bb2def2e85581a663b570f9453380ae067
Maharlikoders <maharlikoders.ph@gmail.com>
2023-10-09 14:22:20 +0800

    Update SketchFileUtil.java

--- app/src/main/java/mod/elfilibustero/sketch/lib/utils/SketchFileUtil.java
+++ app/src/main/java/mod/elfilibustero/sketch/lib/utils/SketchFileUtil.java
@@ -7,8 +7,12 @@ import java.io.File;
 import java.io.RandomAccessFile;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.nio.charset.StandardCharsets;
+
+import a.a.a.oB;
 
 import com.sketchware.pro.R;
+
 import mod.hey.studios.util.Helper;
 
 public class SketchFileUtil {
@@ -17,26 +21,26 @@ public class SketchFileUtil {
     public static final String SKETCHWARE_DIRECTORY = ".sketchware-pro";
     public static final String SKETCHWARE_WORKSPACE_DIRECTORY = SKETCHWARE_DIRECTORY + File.separator + "workspace";
 
+    private oB fileUtil = new oB();
+
     public static String decrypt(String path) throws Exception {
         byte[] fileData = Files.readAllBytes(Paths.get(path));
-        return new String(getCipher(true).doFinal(fileData));
+        return fileUtil.a(fileData);
     }
 
     public static String encrypt(String text) throws Exception {
-        return new String(getCipher(false).doFinal(text.getBytes()));
+        return new String(fileUtil.d(text));
     }
 
     public static boolean encrypt(String text, String path) throws Exception {
-        new RandomAccessFile(path, "rw").write(getCipher(false).doFinal(text.getBytes()));
+        new RandomAccessFile(path, "rw").write(getCipher(Cipher.ENCRYPT_MODE).doFinal(text.getBytes(StandardCharsets.UTF_8)));
         return true;
     }
 
-    private static Cipher getCipher(boolean isDecrypt) throws Exception {
+    private static Cipher getCipher(int mode) throws Exception {
         Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
         byte[] keyBytes = SKETCHWARE_SECURE.getBytes();
-        SecretKeySpec spec = new SecretKeySpec(keyBytes, "AES");
-        IvParameterSpec paramSpec = new IvParameterSpec(keyBytes);
-        cipher.init(isDecrypt ? Cipher.DECRYPT_MODE : Cipher.ENCRYPT_MODE, spec, paramSpec);
+        cipher.init(mode , new SecretKeySpec(keyBytes, "AES"), new IvParameterSpec(keyBytes));
         return cipher;
     }
 }
