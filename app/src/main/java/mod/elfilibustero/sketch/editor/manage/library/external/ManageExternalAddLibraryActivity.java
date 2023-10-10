2871cb7cbcf804c0207cb34d1ac3b661db5b11ac
Maharlikoders <maharlikoders.ph@gmail.com>
2023-10-10 17:12:41 +0800

    Update ManageExternalAddLibraryActivity.java

--- app/src/main/java/mod/elfilibustero/sketch/editor/manage/library/external/ManageExternalAddLibraryActivity.java
+++ app/src/main/java/mod/elfilibustero/sketch/editor/manage/library/external/ManageExternalAddLibraryActivity.java
@@ -4,10 +4,14 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.material.appbar.MaterialToolbar;
 
@@ -18,6 +22,7 @@ import com.sketchware.pro.R;
 import com.sketchware.pro.databinding.ManageExternalAddLibraryBinding;
 
 import mod.SketchwareUtil;
+import mod.elfilibustero.sketch.beans.DependencyBean;
 import mod.elfilibustero.sketch.beans.ExternalLibraryBean;
 import mod.agus.jcoderz.lib.FileUtil;
 import mod.elfilibustero.sketch.lib.handler.ExternalLibraryHandler;
